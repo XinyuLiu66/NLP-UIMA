fb43874001a148ca70dad3e1e3b732d6d4801c66
XinyuLiu66 <449758210@qq.com>
2016-11-27 18:00:46 +0100

    tut3-add the number of tokens in one sentence

---
+++ NLP und web_WS16:17/tut3/src/main/java/tut2/PipelineTutorial2.java
@@ -0,0 +1,49 @@
+package tut2;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.factory.CollectionReaderFactory.createReader;
+
+import java.io.IOException;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.collection.CollectionReader;
+import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
+import org.apache.uima.fit.pipeline.SimplePipeline;
+
+import de.tudarmstadt.ukp.dkpro.core.jazzy.JazzyChecker;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+//import de.tudarmstadt.ukp.dkpro.core.tokit.WhitespaceTokenizer;
+import tut2.reader.WebpageReader;
+import tut2.tokenizer.SentenceSplitter;
+import tut2.tokenizer.TokenizerExample;
+import tut2.tokenizer.WhitespaceTokenizer;
+import tut2.writer.AnnotationWriter;
+import tut2.writer.TokensPerSentenceWriter;
+
+public class PipelineTutorial2 {
+
+	public static void main(String[] args) throws UIMAException, IOException {
+		
+		//CollectionReader textReader = createReader(TextReader.class,
+	   // TextReader.PARAM_DIRECTORY_NAME, "src/main/resources/simple-documents");
+		
+		CollectionReader webpageReader = createReader(WebpageReader.class,
+				WebpageReader.PARAM_URL, "http://www.foxnews.com/", WebpageReader.PARAM_LANGUAGE,"en");
+		
+	//	AnalysisEngine breakIterator = createEngine(BreakIteratorSegmenter.class);
+
+	//	AnalysisEngine stanfordSegmenter = createEngine(StanfordSegmenter.class);
+		AnalysisEngine WhitespaceTokenizer = createEngine(WhitespaceTokenizer.class);
+		AnalysisEngine myTokenizer = createEngine(TokenizerExample.class);
+		
+		AnalysisEngine sentenceSplitter = createEngine(SentenceSplitter.class);
+		AnalysisEngine TokensPerSentenceWriter = createEngine(TokensPerSentenceWriter.class);
+		
+		AnalysisEngine spellChecker = createEngine(JazzyChecker.class,
+				JazzyChecker.PARAM_MODEL_LOCATION, "src/main/resources/dict/words.small");
+
+		AnalysisEngine annotationWriter = createEngine(AnnotationWriter.class);
+		
+		SimplePipeline.runPipeline(/*textReader,*/ webpageReader,sentenceSplitter,WhitespaceTokenizer,myTokenizer,/*breakIterator,*/ spellChecker, annotationWriter);
+	}
+}
